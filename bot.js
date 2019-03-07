2208a7add5eb04edf17fd67ab30cc02b666adb7c
mustaphachafaai <48251244+mustaphachafaai@users.noreply.github.com>
2019-03-07 19:00:46 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ZBI CHINWI`,"http://twitch.tv/HafidHimmi")
+client.user.setGame(`ŤĤĔ ČĤĨŃŴĨ Ď'Ź฿Ĩ ♥`,"http://twitch.tv/HafidHimmi")
 client.user.setStatus("dnd")
 });
 
