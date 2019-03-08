a9b8dc9f0de32ea76671bafd531fef51ec6871e2
mustaphachafaai <48251244+mustaphachafaai@users.noreply.github.com>
2019-03-08 11:22:36 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,6 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`ŤĤĔ ČĤĨŃŴĨ Ď'Ź฿Ĩ ♥`,"http://twitch.tv/chinwi")
 client.user.setStatus("dnd")
-client.user.setsmall_image: `377480550207717376`,
 });
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
